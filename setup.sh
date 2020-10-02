a9624f6ceeaa12adaaa56e1935a75c71d5250650
RubenAMtz <ruben.amtz@outlook.com>
2020-10-01 22:27:20 -0700

    removed uninstall.sh and moved the content to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+sudo pip uninstall jupyter-client==6.1.7 
+sudo pip uninstall jupyter-core==4.6.3 
+sudo pip uninstall jupyterlab-pygments==0.1.2
+sudo pip uninstall markdown==3.2.2
+sudo pip uninstall ipykernel==5.3.4
+sudo pip uninstall tensorboard==2.3.0
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
